4d5cda74ecb32c1183fe5d881c921c1e309dcdcf
Lena <meowcodes@gmail.com>
2020-05-13 13:31:06 -0700

    update as per comments

--- datahub/server/datasources/admin.py
+++ datahub/server/datasources/admin.py
@@ -13,6 +13,7 @@ from logic import user as user_logic
 from logic import environment as environment_logic
 from logic import schedule as schedule_logic
 from logic import metastore as metastore_logic
+from logic import demo as demo_logic
 from models.admin import Announcement, QueryMetastore, QueryEngine, AdminAuditLog
 from models.schedule import TaskSchedule
 
@@ -461,49 +462,49 @@ def exec_demo_set_up():
             session=session,
         ).id
 
-        if environment and metastore_id and engine_id:
-            task_schedule_id = TaskSchedule.create(
-                {
-                    "name": "update_metastore_{}".format(metastore_id),
-                    "task": "tasks.update_metastore.update_metastore",
-                    "cron": "0 0 * * *",
-                    "args": [metastore_id],
-                    "task_type": "prod",
-                    "enabled": True,
-                },
-                # commit=False,
-                session=session,
-            ).id
-            schedule_logic.run_and_log_scheduled_task(
-                scheduled_task_id=task_schedule_id, wait_to_finish=True, session=session
-            )
+        task_schedule_id = TaskSchedule.create(
+            {
+                "name": "update_metastore_{}".format(metastore_id),
+                "task": "tasks.update_metastore.update_metastore",
+                "cron": "0 0 * * *",
+                "args": [metastore_id],
+                "task_type": "prod",
+                "enabled": True,
+            },
+            # commit=False,
+            session=session,
+        ).id
+        schedule_logic.run_and_log_scheduled_task(
+            scheduled_task_id=task_schedule_id, wait_to_finish=True, session=session
+        )
 
-            golden_table = metastore_logic.get_table_by_name(
-                schema_name="main",
-                name="world_happiness_2019",
-                metastore_id=metastore_id,
-                session=session,
+        golden_table = metastore_logic.get_table_by_name(
+            schema_name="main",
+            name="world_happiness_2019",
+            metastore_id=metastore_id,
+            session=session,
+        )
+        if golden_table:
+            metastore_logic.update_table(
+                id=golden_table.id, golden=True, session=session
             )
-            if golden_table:
-                metastore_logic.update_table(
-                    id=golden_table.id, golden=True, session=session
-                )
-                metastore_logic.update_table_information(
-                    data_table_id=golden_table.id,
-                    description="The World Happiness Report is a landmark survey of the state of global happiness. The first report was published in 2012, the second in 2013, the third in 2015, and the fourth in the 2016 Update. The World Happiness 2017, which ranks 155 countries by their happiness levels, was released at the United Nations at an event celebrating International Day of Happiness on March 20th. The report continues to gain global recognition as governments, organizations and civil society increasingly use happiness indicators to inform their policy-making decisions. Leading experts across fields – economics, psychology, survey analysis, national statistics, health, public policy and more – describe how measurements of well-being can be used effectively to assess the progress of nations. The reports review the state of happiness in the world today and show how the new science of happiness explains personal and national variations in happiness.",
-                    session=session,
-                )
-            schedule_logic.run_and_log_scheduled_task(
-                scheduled_task_id=task_schedule_id, session=session
-            )
-
-            data_doc_id = logic.create_demo_data_doc(
-                environment_id=environment.id,
-                engine_id=engine_id,
-                uid=current_user.id,
+            metastore_logic.update_table_information(
+                data_table_id=golden_table.id,
+                description="The World Happiness Report is a landmark survey of the state of global happiness. The first report was published in 2012, the second in 2013, the third in 2015, and the fourth in the 2016 Update. The World Happiness 2017, which ranks 155 countries by their happiness levels, was released at the United Nations at an event celebrating International Day of Happiness on March 20th. The report continues to gain global recognition as governments, organizations and civil society increasingly use happiness indicators to inform their policy-making decisions. Leading experts across fields – economics, psychology, survey analysis, national statistics, health, public policy and more – describe how measurements of well-being can be used effectively to assess the progress of nations. The reports review the state of happiness in the world today and show how the new science of happiness explains personal and national variations in happiness.",
                 session=session,
             )
+        schedule_logic.run_and_log_scheduled_task(
+            scheduled_task_id=task_schedule_id, session=session
+        )
 
+        data_doc_id = demo_logic.create_demo_data_doc(
+            environment_id=environment.id,
+            engine_id=engine_id,
+            uid=current_user.id,
+            session=session,
+        )
+
+        if data_doc_id:
             session.commit()
 
             return {
