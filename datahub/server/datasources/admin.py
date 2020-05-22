3fbcf6a4a0ce4c53f1052674c01d91f723b67791
Lena <meowcodes@gmail.com>
2020-05-22 16:04:30 -0700

    clean up

--- datahub/server/datasources/admin.py
+++ datahub/server/datasources/admin.py
@@ -428,7 +428,7 @@ def get_api_access_tokens_admin():
 def exec_demo_set_up():
     with DBSession() as session:
         environment = environment_logic.create_environment(
-            name="xdemo_environment",
+            name="demo_environment",
             description="Demo environment",
             image="",
             public=True,
@@ -439,7 +439,7 @@ def exec_demo_set_up():
         local_db_conn = "sqlite:///demo/demo_data.db"
         metastore_id = QueryMetastore.create(
             {
-                "name": "zdemo_metastore",
+                "name": "demo_metastore",
                 "metastore_params": {"connection_string": local_db_conn,},
                 "loader": "SqlAlchemyMetastoreLoader",
                 "acl_control": {},
@@ -450,7 +450,7 @@ def exec_demo_set_up():
 
         engine_id = QueryEngine.create(
             {
-                "name": "zsqlite",
+                "name": "sqlite",
                 "description": "SQLite Engine",
                 "language": "sqlite",
                 "executor": "sqlalchemy",
