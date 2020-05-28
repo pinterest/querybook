c5551cbee765ef89b9ca6c0e4769acb9addc5edd
Charlie Gu <cgu@pinterest.com>
2020-05-28 10:56:59 -0400

    Add unit test

--- datahub/tests/test_app/test_server.py
+++ datahub/tests/test_app/test_server.py
@@ -1,3 +1,4 @@
+import os
 from app.server import get_health_check
 
 get_health_check
@@ -5,6 +6,16 @@ get_health_check
 
 def test_ping_api(flask_client):
     assert b"pong" == flask_client.get("/ping/").get_data()
+
+
+def test_ping_deploy_api(flask_client, monkeypatch):
+    with monkeypatch.context() as m:
+        m.setattr(
+            os.path,
+            "exists",
+            lambda p: True if p == "/tmp/datahub/deploying" else False,
+        )
+        assert 503 == flask_client.get("/ping/").status_code
 
 
 def test_four_oh_four(flask_client, fake_user):
