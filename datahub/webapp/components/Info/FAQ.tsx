90bedd9693f88e20f4c8c6026711d33d3303d89f
Lena <meowcodes@gmail.com>
2020-05-28 18:56:01 -0700

    faq outline

--- datahub/webapp/components/Info/FAQ.tsx
+++ datahub/webapp/components/Info/FAQ.tsx
@@ -1,7 +1,25 @@
 import * as React from 'react';
+import { Title } from 'ui/Title/Title';
 
-interface IProps {}
+import './FAQ.scss';
 
-export const FAQ: React.FunctionComponent<IProps> = () => {
-    return null;
+const faqs = [['Question?', 'answer']];
+
+export const FAQ: React.FunctionComponent = () => {
+    return (
+        <div className="FAQ">
+            {faqs.map((faq, idx) => {
+                const question = faq[0];
+                const answer = faq[1];
+                return (
+                    <div className="FAQ-item mb8" key={idx}>
+                        <Title subtitle size={4} className="FAQ-question">
+                            {question}
+                        </Title>
+                        <div className="FAQ-answer mv8 mh12">{answer}</div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
