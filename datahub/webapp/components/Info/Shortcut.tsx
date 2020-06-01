d7f9388ede6158c66ad88fd811160e6b6d86b31f
Lena <meowcodes@gmail.com>
2020-06-01 16:53:40 -0700

    update

--- datahub/webapp/components/Info/Shortcut.tsx
+++ datahub/webapp/components/Info/Shortcut.tsx
@@ -5,161 +5,41 @@ import { Title } from 'ui/Title/Title';
 
 import './Shortcut.scss';
 
+const shortcuts = require('config/shortcuts.yaml').shortcuts;
+
 export const Shortcut: React.FunctionComponent = () => {
     return (
         <div className="Shortcut">
-            <div className="Shortcut-box">
-                <Title subtitle size={3}>
-                    overall UI
-                </Title>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>⌘</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>k</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">open search</div>
+            {shortcuts.map((box) => (
+                <div className="Shortcut-box" key={box.title}>
+                    <Title subtitle size={3}>
+                        {box.title}
+                    </Title>
+                    {box.keys.map((shortcut, idx) => {
+                        const [keys, text] = shortcut;
+                        return (
+                            <div className="Shortcut-item" key={idx}>
+                                <div className="Shortcut-keys">
+                                    {keys.map((key, i) => {
+                                        if (i === 0) {
+                                            return (
+                                                <KeyboardKey>{key}</KeyboardKey>
+                                            );
+                                        }
+                                        return (
+                                            <>
+                                                <span className="pr4">+</span>
+                                                <KeyboardKey>{key}</KeyboardKey>
+                                            </>
+                                        );
+                                    })}
+                                </div>
+                                <div className="Shortcut-text">{text}</div>
+                            </div>
+                        );
+                    })}
                 </div>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>⌘</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>b</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">toggle sidebar</div>
-                </div>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>esc</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">close modal</div>
-                </div>
-            </div>
-            <div className="Shortcut-box">
-                <Title subtitle size={3}>
-                    data doc and adhoc{' '}
-                </Title>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>⌘</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>s</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">save datadoc</div>
-                </div>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>⌘</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>f</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">open search and replace</div>
-                </div>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>esc</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">
-                        close search and replace
-                    </div>
-                </div>
-            </div>
-            <div className="Shortcut-box">
-                <Title subtitle size={3}>
-                    rich text
-                </Title>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>⌘</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>k</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">add link</div>
-                </div>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>⌘</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>b</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">bold</div>
-                </div>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>⌘</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>i</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">italics</div>
-                </div>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>⌘</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>shift</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>x</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">strikethrough</div>
-                </div>
-            </div>
-            <div className="Shortcut-box">
-                <Title subtitle size={3}>
-                    query editor
-                </Title>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>shift</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>enter</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">run query </div>
-                </div>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>ctrl</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>space</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">force autocompletion</div>
-                </div>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>shift</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>tab</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">indent less</div>
-                </div>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>⌘</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>/</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">toggle comment</div>
-                </div>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>⌘</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>p</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">
-                        open table modal if cursor is on a table
-                    </div>
-                </div>
-                <div className="Shortcut-item">
-                    <div className="Shortcut-keys">
-                        <KeyboardKey>shift</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>alt</KeyboardKey>
-                        <span className="pr4">+</span>
-                        <KeyboardKey>f</KeyboardKey>
-                    </div>
-                    <div className="Shortcut-text">format query</div>
-                </div>
-            </div>
+            ))}
         </div>
     );
 };
