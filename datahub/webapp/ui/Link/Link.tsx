ca32a25ca58fb9e7e475584f05ff8433e1956330
Lena <meowcodes@gmail.com>
2020-06-08 12:50:08 -0700

    update

--- datahub/webapp/ui/Link/Link.tsx
+++ datahub/webapp/ui/Link/Link.tsx
@@ -23,7 +23,7 @@ function isInternalUrl(url: LocationDescriptor): boolean {
 
 export interface ILinkProps {
     to?: LocationDescriptor;
-    onClick?: (to: any) => any;
+    onClick?: (to: React.MouseEvent) => any;
     newTab?: boolean;
 
     className?: string;
