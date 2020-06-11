6f30bbdff02a16ab9b305cd3d9160e5844690788
Lena <meowcodes@gmail.com>
2020-06-11 13:56:13 -0700

    [UI] Fix Table of Contents Overflow

--- datahub/webapp/components/DataDocLeftSidebar/DataDocContents.tsx
+++ datahub/webapp/components/DataDocLeftSidebar/DataDocContents.tsx
@@ -7,6 +7,8 @@ import { DraggableList } from 'ui/DraggableList/DraggableList';
 import { moveDataDocCell } from 'redux/dataDoc/action';
 import { getScrollParent } from 'lib/utils';
 
+import './DataDocContents.scss';
+
 const CELL_TEXT_LEN = 50;
 
 export const DataDocContents: React.FunctionComponent<{
@@ -25,7 +27,7 @@ export const DataDocContents: React.FunctionComponent<{
         return ret;
     }, [cells]);
     return (
-        <div ref={selfRef}>
+        <div className="DataDocContents" ref={selfRef}>
             <DraggableList
                 items={cells}
                 className="contents-panel-cell-rows"
