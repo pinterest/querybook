8472a9cf02b53c22f30f3ea4e21f2c35dfc581ba
Charlie Gu <cgu@pinterest.com>
2020-06-19 14:16:44 -0400

    support upto 10 chars in environment picker

--- datahub/webapp/const/font.ts
+++ datahub/webapp/const/font.ts
@@ -8,6 +8,7 @@ export const numFontSizeToString = {
     7: 'var(--small-text-size)',
     8: 'var(--xsmall-text-size)',
     9: 'var(--xxsmall-text-size)',
+    10: 'var(--xxxsmall-text-size)',
 };
 
-export type NumFontSize = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+export type NumFontSize = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
