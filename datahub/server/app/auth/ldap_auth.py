4e17b010c8a083ec06c1be1b44025b4ec80381c4
Lena <meowcodes@gmail.com>
2020-07-15 10:59:03 -0700

    ldap

--- datahub/server/app/auth/ldap_auth.py
+++ datahub/server/app/auth/ldap_auth.py
@@ -11,3 +11,35 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import ldap
+from env import DataHubSettings
+
+from .utils import DataHubLoginManager
+
+
+class LDAPLoginManager(object):
+    def __init__(self):
+        self.login_manager = DataHubLoginManager()
+
+    def init_app(self, flask_app):
+        self.conn = ldap.initialize(DataHubSettings.LDAP_CONN)
+        self.conn.set_option(ldap.OPT_REFERRALS, 0)
+        self.conn.simple_bind_s(
+            DataHubSettings.LDAP_LOGIN, DataHubSettings.LDAP_PASSWORD
+        )
+
+    def login(self, request):
+        self.conn.search_s(DataHubSettings.LDAP_DOMAIN, ldap.SCOPE_SUBTREE)
+
+
+login_manager = LDAPLoginManager()
+
+ignore_paths = []
+
+
+def init_app(app):
+    login_manager.init_app(app)
+
+
+def login(request):
+    return login_manager.login(request)
