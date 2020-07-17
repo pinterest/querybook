19d2066f9bec8283081c1bfd70d17fd6e2389155
Lena <meowcodes@gmail.com>
2020-07-17 13:14:03 -0700

    update

--- datahub/server/app/auth/ldap_auth.py
+++ datahub/server/app/auth/ldap_auth.py
@@ -12,29 +12,60 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import ldap
+import flask_login
+
+from flask_login import current_user
 from env import DataHubSettings
-
-from .utils import DataHubLoginManager
-
-
-class LDAPLoginManager(object):
-    def __init__(self):
-        self.login_manager = DataHubLoginManager()
-
-    def init_app(self, flask_app):
-        self.conn = ldap.initialize(DataHubSettings.LDAP_CONN)
-        self.conn.set_option(ldap.OPT_REFERRALS, 0)
-        self.conn.simple_bind_s(
-            DataHubSettings.LDAP_LOGIN, DataHubSettings.LDAP_PASSWORD
-        )
-
-    def login(self, request):
-        self.conn.search_s(DataHubSettings.LDAP_DOMAIN, ldap.SCOPE_SUBTREE)
+from app.db import with_session, DBSession
+from logic.user import get_user_by_name
+from .utils import AuthenticationError, AuthUser, DataHubLoginManager
 
 
-login_manager = LDAPLoginManager()
+login_manager = DataHubLoginManager()
 
-ignore_paths = []
+
+@with_session
+def authenticate(username, password, session=None):
+    """
+    Authenticate a PasswordUser with the specified
+    username/password.
+
+    :param username: The username
+    :param password: The password
+    :param session: An active SQLAlchemy session
+
+
+    :raise AuthenticationError: if an error occurred
+    :return: a PasswordUser
+    """
+    if not username or not password:
+        raise AuthenticationError()
+
+    conn = ldap.initialize(DataHubSettings.LDAP_CONN)
+    conn.set_option(ldap.OPT_REFERRALS, 0)
+    conn.simple_bind_s(DataHubSettings.LDAP_LOGIN, DataHubSettings.LDAP_PASSWORD)
+
+    conn.search_s(DataHubSettings.LDAP_DOMAIN, ldap.SCOPE_SUBTREE)
+
+    user = get_user_by_name(username, session=session)
+    if not user:
+        raise AuthenticationError("User does not exist or wrong password")
+
+    if not user.check_password(password):
+        raise AuthenticationError("User does not exist or wrong password")
+
+    return AuthUser(user)
+
+
+def login_user_endpoint(username, password):
+    if current_user.is_authenticated:
+        return
+
+    with DBSession() as session:
+        user = authenticate(username, password, session=session)
+        flask_login.login_user(user)
+
+        return user
 
 
 def init_app(app):
@@ -42,4 +73,5 @@ def init_app(app):
 
 
 def login(request):
-    return login_manager.login(request)
+    # The webapp will handle the UI for logging in
+    pass
