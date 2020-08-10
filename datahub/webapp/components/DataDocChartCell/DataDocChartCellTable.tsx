68f43900e516c26cec6102b7e92e15f921b985a2
Lena <meowcodes@gmail.com>
2020-08-10 11:43:19 -0700

    [Chart] Add Title to Table Charts (#191)
    closes #186

---
+++ datahub/webapp/components/DataDocChartCell/DataDocChartCellTable.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+import { StatementResultTable } from 'components/DataDocStatementExecution/StatementResultTable';
+import { Title } from 'ui/Title/Title';
+
+interface IProps {
+    title: string;
+    data: any[][];
+}
+
+export const DataDocChartCellTable: React.FunctionComponent<IProps> = ({
+    title,
+    data,
+}) => {
+    return (
+        <div>
+            {title?.length ? (
+                <Title size={6} className="mv4 center-align">
+                    {title}
+                </Title>
+            ) : null}
+            <StatementResultTable
+                data={data}
+                paginate={true}
+                maxNumberOfRowsToShow={20}
+            />
+        </div>
+    );
+};
