5c2ef4178de0571ecc9e885428254fd5b7415fc3
cgu <cgu@pinterest.com>
2020-10-14 16:42:20 -0400

    Add pretty number formatting

--- datahub/webapp/components/DataTableStats/DataTableStatsCommon.tsx
+++ datahub/webapp/components/DataTableStats/DataTableStatsCommon.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import { TableStatValue } from 'const/metastore';
 import { formatNumber } from 'lib/chart/chart-utils';
-import { PrettyNumber } from 'ui/PrettyNumber/PrettyNumber';
 
 export const TableStats: React.FC<{ val: TableStatValue }> = ({ val }) => {
     let dom: React.ReactNode = null;
     if (Array.isArray(val)) {
         dom = val.map((item, idx) => <div key={idx}>{item}</div>);
     } else if (typeof val === 'number') {
-        dom = <PrettyNumber val={val} />;
+        dom = formatNumber(val);
     } else {
         dom = val;
     }
