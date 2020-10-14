b4684d3c9fe7f40328fd7b76b7859690984a9775
cgu <cgu@pinterest.com>
2020-10-14 17:44:03 -0400

    Use DOM for number

--- datahub/webapp/components/DataTableStats/DataTableStatsCommon.tsx
+++ datahub/webapp/components/DataTableStats/DataTableStatsCommon.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { TableStatValue } from 'const/metastore';
 import { formatNumber } from 'lib/chart/chart-utils';
+import { PrettyNumber } from 'ui/PrettyNumber/PrettyNumber';
 
 export const TableStats: React.FC<{ val: TableStatValue }> = ({ val }) => {
     let dom: React.ReactNode = null;
     if (Array.isArray(val)) {
         dom = val.map((item, idx) => <div key={idx}>{item}</div>);
     } else if (typeof val === 'number') {
-        dom = formatNumber(val);
+        dom = <PrettyNumber val={val} />;
     } else {
         dom = val;
     }
