ef9684150ab68ac36d7d3668b940c0dd210eb8a6
czgu <cgu@pinterest.com>
2020-11-03 14:58:46 -0500

    Add documentation for query result transform (#303)
    * Add documentation for query result transform
    
    * Update add_query_result_transform.md

--- datahub/webapp/lib/query-result/types.ts
+++ datahub/webapp/lib/query-result/types.ts
@@ -2,6 +2,7 @@ import React from 'react';
 
 export interface IColumnDetector {
     type: string;
+    // can be any number, higher number gets called first
     priority: number;
     checker: (colName: string, values: any[]) => boolean;
 }
@@ -18,7 +19,11 @@ export interface IColumnTransformer {
     name: string;
 
     appliesToType: string[];
+
+    // this determines the order of transformers
+    // in the menu. Higher priority is shown first
     priority: number;
+    // if auto, then transformer is turned on by default
     auto: boolean;
 
     transform: (v: any) => React.ReactNode;
