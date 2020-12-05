33feac986e48789a91b0e31c8c2a36bfe47aaa3c
cgu <cgu@pinterest.com>
2020-12-04 20:32:06 -0500

    Fixed windowscroll, updated gradient & primary color

--- docs_website/src/hooks/useWindowScroll.js
+++ docs_website/src/hooks/useWindowScroll.js
@@ -1,14 +1,24 @@
 import { useEffect, useState } from 'react';
 
 export default () => {
-    const [scrollY, setScrollY] = useState(window.scrollY);
+    const [scrollY, setScrollY] = useState(() => {
+        if (typeof window !== 'undefined') {
+            return window.scrollY;
+        }
+        return 0;
+    });
     useEffect(() => {
-        const scrollListener = () => {
-            setScrollY(window.scrollY);
-        };
-        document.addEventListener('scroll', scrollListener);
+        let scrollListener;
+        if (typeof window !== 'undefined') {
+            scrollListener = () => {
+                setScrollY(window.scrollY);
+            };
+            document.addEventListener('scroll', scrollListener);
+        }
         return () => {
-            document.removeEventListener('scroll', scrollListener);
+            if (scrollListener) {
+                document.removeEventListener('scroll', scrollListener);
+            }
         };
     }, []);
 
