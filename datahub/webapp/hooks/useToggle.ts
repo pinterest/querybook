c62c1568bdec366a20c1a2054338bda722e86375
cgu <cgu@pinterest.com>
2020-12-12 11:36:05 -0500

    fix linter test

--- datahub/webapp/hooks/useToggle.ts
+++ datahub/webapp/hooks/useToggle.ts
@@ -4,8 +4,8 @@ import React, { useCallback } from 'react';
  * Create a toggle function for a boolean useState setter
  *
  * @param setter the setter function provided by useState
- * @param deps the dependency array if the setter would be recreated
- *             during rerender
+ * @param deps the dependency array if the setter would be recreated during rerender
+ *
  */
 export const useToggle = (
     setter: React.Dispatch<React.SetStateAction<boolean>>,
