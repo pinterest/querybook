7ed19204deca5c2396c9ea62899fa42d02bf1124
cgu <cgu@pinterest.com>
2021-03-24 00:32:21 -0700

    disable wdyr in testing

--- babel.config.js
+++ babel.config.js
@@ -6,9 +6,10 @@ module.exports = (api) => {
             [
                 '@babel/preset-react',
                 {
-                    runtime: 'automatic',
-                    development: !isProd,
-                    importSource: '@welldone-software/why-did-you-render',
+                    // uncomment before to use why-did-you-render
+                    // runtime: 'automatic',
+                    // development: !isProd,
+                    // importSource: '@welldone-software/why-did-you-render',
                 },
             ],
             [
