3b5293c7495ed981d88eddf94e4214c80f148a2d
cgu <cgu@pinterest.com>
2021-03-27 14:38:01 -0700

    Update doc for first setup

--- docs_website/src/components/TryItOut/index.jsx
+++ docs_website/src/components/TryItOut/index.jsx
@@ -12,35 +12,35 @@ export default () => {
                 subtitle="Use the following resources to get a demo instance running in a few minutes."
             />
             <div className=" cards-container">
-                <div class="card">
-                    <div class="card__header">
+                <div className="card">
+                    <div className="card__header">
                         <h3>Step 1: Download</h3>
                     </div>
-                    <div class="card__body">
+                    <div className="card__body">
                         <p>Visit Github to fork/clone the repo.</p>
                     </div>
-                    <div class="card__footer">
+                    <div className="card__footer">
                         <Link
-                            class="button button--secondary button--block"
+                            className="button button--secondary button--block"
                             to={'https://github.com/pinterest/querybook'}
                         >
                             Github
                         </Link>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="card__header">
+                <div className="card">
+                    <div className="card__header">
                         <h3>Step 2: Run</h3>
                     </div>
-                    <div class="card__body">
+                    <div className="card__body">
                         <p>
                             Run <i>make</i> in the root directory to start the
                             demo instance.
                         </p>
                     </div>
-                    <div class="card__footer">
+                    <div className="card__footer">
                         <Link
-                            class="button button--secondary button--block"
+                            className="button button--secondary button--block"
                             to="/docs/setup_guide/quick_setup"
                         >
                             Quick Start Guide
