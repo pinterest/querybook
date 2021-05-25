6094da6604725d31b49ecc66509d489725d0a401
Lena Ryoo <lryoo@pinterest.com>
2021-05-25 15:52:45 -0700

    clean

--- querybook/webapp/ui/ReccurenceEditor/RecurrenceEditor.tsx
+++ querybook/webapp/ui/ReccurenceEditor/RecurrenceEditor.tsx
@@ -11,7 +11,7 @@ import {
     RecurrenceType,
     recurrenceTypes,
     getYearlyMonthOptions,
-    IRecurrenceOn
+    IRecurrenceOn,
 } from 'lib/utils/cron';
 import { makeReactSelectStyle } from 'lib/utils/react-select';
 
@@ -31,7 +31,7 @@ interface IProps {
 export const RecurrenceEditor: React.FunctionComponent<IProps> = ({
     recurrence,
     recurrenceError,
-    setRecurrence
+    setRecurrence,
 }) => {
     const localTime = React.useMemo(
         () => getRecurrenceLocalTimeString(recurrence),
@@ -54,7 +54,7 @@ export const RecurrenceEditor: React.FunctionComponent<IProps> = ({
                         const newRecurrence = {
                             ...recurrence,
                             hour: value.hour(),
-                            minute: value.minute()
+                            minute: value.minute(),
                         };
                         setRecurrence(newRecurrence);
                     }}
@@ -77,7 +77,7 @@ export const RecurrenceEditor: React.FunctionComponent<IProps> = ({
                         onSelect={(key: RecurrenceType) => {
                             const newRecurrence = {
                                 ...recurrence,
-                                recurrence: key
+                                recurrence: key,
                             };
                             if (field.value !== key) {
                                 newRecurrence.on = {};
@@ -164,7 +164,7 @@ export const RecurrenceEditorDatePicker: React.FunctionComponent<IDatePickerProp
     options,
     error,
     recurrence,
-    setRecurrence
+    setRecurrence,
 }) => {
     const formattedError = (error?.[onKey] || '').replace(
         `recurrence.on.${onKey}`,
@@ -190,8 +190,8 @@ export const RecurrenceEditorDatePicker: React.FunctionComponent<IDatePickerProp
                                     ...recurrence.on,
                                     [onKey]: (value as Array<{
                                         value: any;
-                                    }>).map((v) => v.value)
-                                }
+                                    }>).map((v) => v.value),
+                                },
                             };
                             setRecurrence(newRecurrence);
                         }}
