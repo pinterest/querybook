80521d8294fb1ece20c4788372c75d1e2289cb7c
satish.iyengar <satish.iyengar@epicgames.com>
2021-06-08 15:45:39 -0400

    feat: support progressPercentage in hive thrift response (activated pre-commit hook)

--- querybook/tests/test_lib/test_query_executor/test_clients/test_hive_progress_percentage.py
+++ querybook/tests/test_lib/test_query_executor/test_clients/test_hive_progress_percentage.py
@@ -1,14 +1,19 @@
 from unittest import TestCase
 from lib.query_executor.clients.hive import HiveCursor
-from TCLIService.ttypes import *
-import json
+from TCLIService.ttypes import (
+    TProgressUpdateResp,
+    TGetOperationStatusResp,
+    TOperationState,
+)
 
 
 class ProgressPercentageTestCase(TestCase):
     def test_progress_percentage_reported(self):
         progress_resp = TProgressUpdateResp(progressedPercentage=0.65)
-        resp = TGetOperationStatusResp(operationState=TOperationState.RUNNING_STATE,
-                                       progressUpdateResponse=progress_resp)
+        resp = TGetOperationStatusResp(
+            operationState=TOperationState.RUNNING_STATE,
+            progressUpdateResponse=progress_resp,
+        )
         hive_cursor = HiveCursor(None)
         hive_cursor._update_percent_complete(resp)
         self.assertEqual(hive_cursor._percent_complete, 65)
@@ -20,10 +25,13 @@ class ProgressPercentageTestCase(TestCase):
         self.assertEqual(hive_cursor._percent_complete, 0)
 
     def test_progress_old_hive_version(self):
-        task_status_json = '[{"taskType": "MAPRED", "mapProgress": 50, "reduceProgress": 0 }]'
-        resp = TGetOperationStatusResp(operationState=TOperationState.RUNNING_STATE,
-                                       taskStatus=task_status_json)
+        # mock status json
+        task_status_json = (
+            '[{"taskType": "MAPRED", "mapProgress": 50, "reduceProgress": 0 }]'
+        )
+        resp = TGetOperationStatusResp(
+            operationState=TOperationState.RUNNING_STATE, taskStatus=task_status_json
+        )
         hive_cursor = HiveCursor(None)
         hive_cursor._update_percent_complete(resp)
         self.assertEqual(hive_cursor._percent_complete, 25)
-
