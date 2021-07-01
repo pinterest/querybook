8eae4f23a0036f070e00afb16d196091b263956b
cgu <cgu@pinterest.com>
2021-07-01 13:31:26 -0700

    fix landing page redirect

--- querybook/webapp/components/EnvironmentAppRouter/mainRoute/DataDocRoute.tsx
+++ querybook/webapp/components/EnvironmentAppRouter/mainRoute/DataDocRoute.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Route, Switch, RouteComponentProps } from 'react-router-dom';
+import { Route, Switch, RouteComponentProps, Redirect } from 'react-router-dom';
 
 import { DataDocWrapper } from 'components/DataDoc/DataDocWrapper';
-import { Landing } from 'components/Landing/Landing';
 
 import { FourOhFour } from 'ui/ErrorPage/FourOhFour';
 
 const DataDocRoute: React.FunctionComponent<RouteComponentProps> = () => (
     <Switch>
         <Route path="/:env/datadoc/:docId/" component={DataDocWrapper} />
-        <Route path="/:env/datadoc/" exact={true} component={Landing} />
+        <Route path="/:env/datadoc/" exact={true}>
+            <Redirect to="/" />
+        </Route>
         <Route component={FourOhFour} />
     </Switch>
 );
