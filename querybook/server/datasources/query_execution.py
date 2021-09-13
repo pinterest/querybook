d61e4f5940bd22b43d966fb2e666167c5fb613be
kgopal <kgopal@pinterest.com>
2021-09-13 10:24:29 -0700

    make engine_id variable required

--- querybook/server/datasources/query_execution.py
+++ querybook/server/datasources/query_execution.py
@@ -374,7 +374,7 @@ def export_statement_execution_result(
 
 
 @register("/query_execution/templated_query/", methods=["POST"])
-def get_templated_query(query: str, variables: Dict[str, str], engine_id: int = None):
+def get_templated_query(query: str, variables: Dict[str, str], engine_id: int):
     try:
         return render_templated_query(query, variables, engine_id)
     except QueryTemplatingError as e:
