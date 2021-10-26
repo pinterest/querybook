a67296c781bb46aedb6bcdd0b63da886d789727d
czgu <cgu@pinterest.com>
2021-10-26 17:51:28 -0400

    fix: truncate information from metastore + small ui fixes (#684)
    https://github.com/pinterest/querybook/pull/684

--- querybook/server/models/metastore.py
+++ querybook/server/models/metastore.py
@@ -11,7 +11,7 @@ from const.db import (
     mediumtext_length,
 )
 from const.metastore import DataTableWarningSeverity
-from lib.sqlalchemy import CRUDMixin
+from lib.sqlalchemy import CRUDMixin, TruncateString
 
 Base = db.Base
 
@@ -120,7 +120,7 @@ class DataJobMetadata(Base):
         return complete_dict
 
 
-class DataSchema(Base):
+class DataSchema(TruncateString("name"), Base):
     __tablename__ = "data_schema"
 
     id = sql.Column(sql.Integer, primary_key=True)
@@ -162,7 +162,7 @@ class DataSchema(Base):
         return schema_dict
 
 
-class DataTable(Base, CRUDMixin):
+class DataTable(CRUDMixin, TruncateString("name", "type", "location"), Base):
     __tablename__ = "data_table"
 
     id = sql.Column(sql.Integer, primary_key=True)
@@ -241,7 +241,9 @@ class DataTable(Base, CRUDMixin):
         return table
 
 
-class DataTableInformation(Base):
+class DataTableInformation(
+    TruncateString("latest_partitions", "earliest_partitions"), Base
+):
     __tablename__ = "data_table_information"
     __table_args__ = {"mysql_engine": "InnoDB", "mysql_charset": "utf8mb4"}
 
@@ -268,7 +270,7 @@ class DataTableInformation(Base):
         return self.description
 
 
-class DataTableColumn(Base):
+class DataTableColumn(TruncateString("name", "type", "comment"), Base):
     __tablename__ = "data_table_column"
 
     id = sql.Column(sql.Integer, primary_key=True)
@@ -325,7 +327,7 @@ class DataTableOwnership(Base):
         return item
 
 
-class DataTableQueryExecution(Base, CRUDMixin):
+class DataTableQueryExecution(CRUDMixin, Base):
     __tablename__ = "data_table_query_execution"
 
     id = sql.Column(sql.Integer, primary_key=True, autoincrement=True)
@@ -360,7 +362,7 @@ class DataTableQueryExecution(Base, CRUDMixin):
     )
 
 
-class DataTableWarning(Base, CRUDMixin):
+class DataTableWarning(CRUDMixin, Base):
     __tablename__ = "data_table_warnings"
 
     id = sql.Column(sql.Integer, primary_key=True, autoincrement=True)
@@ -382,7 +384,7 @@ class DataTableWarning(Base, CRUDMixin):
     )
 
 
-class DataTableStatistics(Base, CRUDMixin):
+class DataTableStatistics(CRUDMixin, Base):
     __tablename__ = "data_table_statistics"
 
     id = sql.Column(sql.Integer, primary_key=True)
@@ -404,7 +406,7 @@ class DataTableStatistics(Base, CRUDMixin):
     )
 
 
-class DataTableColumnStatistics(Base, CRUDMixin):
+class DataTableColumnStatistics(CRUDMixin, Base):
     __tablename__ = "data_table_column_statistics"
 
     id = sql.Column(sql.Integer, primary_key=True)
