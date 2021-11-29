dd9eb048b8c9cbc7ce2a29bacd1c37bb4594d2e1
czgu <cgu@pinterest.com>
2021-11-29 10:22:56 -0800

    chore: update to ts 4.4.4 (#707)
    * chore: update to ts 4.4.4
    
    * yarn install
    
    * update @types/ versions, rebase
    
    * fix axios error test
    
    * fix tsc

--- querybook/webapp/__tests__/lib/utils/error.test.ts
+++ querybook/webapp/__tests__/lib/utils/error.test.ts
@@ -2,6 +2,7 @@ import { formatError } from 'lib/utils/error';
 
 test('format error object', () => {
     const testErrorObj = new Error();
+    testErrorObj['isAxiosError'] = true;
     testErrorObj['response'] = { data: { error: 'test error message' } };
 
     expect(formatError(testErrorObj)).toBe('test error message');
--- querybook/webapp/lib/utils/error.ts
+++ querybook/webapp/lib/utils/error.ts
@@ -10,17 +10,19 @@ export function formatError(error: any): string {
         typeof error === 'object' &&
         error.constructor === Error;
     if (isErrorObject) {
-        if (
-            error.response &&
-            error.response.data &&
-            typeof error.response.data === 'object'
-        ) {
-            // The request was made and the server responded with a status code > 2xx
-            if ('error' in error.response.data) {
-                return error.response.data.error;
+        if (isAxiosError(error)) {
+            if (
+                error.response &&
+                error.response.data &&
+                typeof error.response.data === 'object'
+            ) {
+                // The request was made and the server responded with a status code > 2xx
+                if ('error' in error.response.data) {
+                    return error.response.data.error;
+                }
+            } else if (error.request) {
+                // The request was made but no response was received
             }
-        } else if (error.request) {
-            // The request was made but no response was received
         } else {
             // unknown error, maybe syntax?
         }
