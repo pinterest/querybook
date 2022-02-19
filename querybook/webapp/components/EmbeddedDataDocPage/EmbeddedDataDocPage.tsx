025a0c9822a1892d8dc63eddd37a7c735f5c8411
Kartik Darapuneni <kartik.darapuneni@grandrounds.com>
2022-02-18 19:11:39 -0800

    CSS is used to enable scrolling

--- querybook/webapp/components/EmbeddedDataDocPage/EmbeddedDataDocPage.tsx
+++ querybook/webapp/components/EmbeddedDataDocPage/EmbeddedDataDocPage.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { FullHeight } from 'ui/FullHeight/FullHeight';
+import './EmbeddedDataDocPage.scss';
 import { DataDoc } from 'components/DataDoc/DataDoc';
 
 const EmbeddedDataDocPage: React.FunctionComponent = ({id}) => {
     return (
         <FullHeight flex={'column'} className="EmbeddedDataDocPage">
-            <DataDoc docId={id} />
+            <div className="datadoc-wrapper">
+                <DataDoc docId={id} />
+            </div>
         </FullHeight>
     );
 };
