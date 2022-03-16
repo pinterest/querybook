f1a25dd81a7f9c0a15cc67da2e5a02fb72a45bdd
Kartik Darapuneni <kartik.darapuneni@grandrounds.com>
2022-03-15 20:46:04 -0700

    lint

--- querybook/webapp/lib/utils/embed.ts
+++ querybook/webapp/lib/utils/embed.ts
@@ -1,3 +1,4 @@
 const EMBED_PATH_STRING = 'embedded';
 
-export const isEmbedPath = (path: string): boolean => path.includes(EMBED_PATH_STRING);
+export const isEmbedPath = (path: string): boolean =>
+    path.includes(EMBED_PATH_STRING);
