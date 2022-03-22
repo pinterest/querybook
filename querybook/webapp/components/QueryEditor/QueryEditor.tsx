9fff8217eb6d45c0feff7cb2ef958efb6ff904dc
czgu <cgu@pinterest.com>
2022-03-21 17:26:07 -0700

    feat: add ability to swap lines in QueryEditor (#782)
    * feat: add ability to swap lines
    
    * fix comment

--- querybook/webapp/components/QueryEditor/QueryEditor.tsx
+++ querybook/webapp/components/QueryEditor/QueryEditor.tsx
@@ -215,6 +215,9 @@ export class QueryEditor extends React.PureComponent<
                 [KeyMap.queryEditor.autocomplete.key]: 'autocomplete',
                 [KeyMap.queryEditor.indentLess.key]: 'indentLess',
                 [KeyMap.queryEditor.toggleComment.key]: 'toggleComment',
+                [KeyMap.queryEditor.swapLineUp.key]: 'swapLineUp',
+                [KeyMap.queryEditor.swapLineDown.key]: 'swapLineDown',
+
                 [KeyMap.queryEditor.openTable.key]: this.onOpenTableModal,
                 [KeyMap.queryEditor.formatQuery.key]: this.formatQuery,
                 ...keyMap,
