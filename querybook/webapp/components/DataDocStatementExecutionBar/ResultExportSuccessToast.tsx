876b89fd4d6188f61ef285354b755eae12955311
Lena Ryoo <lryoo@pinterest.com>
2022-03-24 13:08:33 -0700

    ui: add margin

--- querybook/webapp/components/DataDocStatementExecutionBar/ResultExportSuccessToast.tsx
+++ querybook/webapp/components/DataDocStatementExecutionBar/ResultExportSuccessToast.tsx
@@ -4,7 +4,6 @@ import { Button } from 'ui/Button/Button';
 import { Modal } from 'ui/Modal/Modal';
 import { CopyPasteModal } from 'ui/CopyPasteModal/CopyPasteModal';
 import { IQueryExecutionExportStatusInfo } from 'const/queryExecution';
-import history from 'lib/router-history';
 import toast from 'react-hot-toast';
 
 const UrlModal: React.FunctionComponent<{
@@ -23,7 +22,7 @@ export const ResultExportSuccessToast = (
     data: IQueryExecutionExportStatusInfo
 ) => {
     const { result: exportedInfo, task_id: taskId } = data;
-    const title = 'Completed exporting statement execution results!';
+    const title = 'Export Complete';
 
     return exportedInfo.type === 'url' ? (
         <UrlModal
