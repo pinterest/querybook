bb6c52e59ef5dd2998b7139182c03d12cc6b4bc3
czgu <cgu@pinterest.com>
2022-03-30 14:14:41 -0700

    fix: update limiter logic, frontend error format limiter errors (#816)
    * fix: update limiter logic, frontend error format limiter errors
    
    * remove rate limit handler in datasource as it is not used

--- querybook/webapp/lib/utils/error.ts
+++ querybook/webapp/lib/utils/error.ts
@@ -1,4 +1,6 @@
 import { AxiosError } from 'axios';
+import moment from 'moment';
+import { formatDuration, generateFormattedDate } from './datetime';
 
 export function formatError(error: any): string {
     if (typeof error === 'string') {
@@ -11,17 +13,24 @@ export function formatError(error: any): string {
         error.constructor === Error;
     if (isErrorObject) {
         if (isAxiosError(error)) {
-            if (
-                error.response &&
-                error.response.data &&
-                typeof error.response.data === 'object'
-            ) {
-                // The request was made and the server responded with a status code > 2xx
-                if ('error' in error.response.data) {
-                    return error.response.data.error;
+            if (error.response) {
+                if (
+                    error.response.data &&
+                    typeof error.response.data === 'object'
+                ) {
+                    // The request was made and the server responded with a status code > 2xx
+                    if ('error' in error.response.data) {
+                        return error.response.data.error;
+                    }
                 }
-            } else if (error.request) {
-                // The request was made but no response was received
+
+                if (
+                    error.response.status === 429 &&
+                    'flask-limit-key' in error.response.headers
+                ) {
+                    // Rate limit error from Flask-Limiter
+                    return formatRateLimitError(error.response.headers);
+                }
             }
         } else {
             // unknown error, maybe syntax?
@@ -33,4 +42,21 @@ export function formatError(error: any): string {
 
 export function isAxiosError(e: any): e is AxiosError {
     return e instanceof Error && (e as AxiosError).isAxiosError;
+}
+
+function formatRateLimitError(headers: Record<string, string>) {
+    const limitKey = headers['flask-limit-key'];
+    const limitAmount = headers['flask-limit-amount'];
+    const limitWindowSize = Number(headers['flask-limit-window-size']);
+    const limitResetAt = Number(headers['flask-limit-reset-at']);
+
+    const exceededMessage = `Rate limit ${limitAmount} requests per ${formatDuration(
+        moment.duration(limitWindowSize, 'seconds')
+    )} exceeded.`;
+    const retryMessage = `Please retry after ${generateFormattedDate(
+        limitResetAt,
+        'X'
+    )}.`;
+
+    return `${exceededMessage} ${retryMessage} (key: ${limitKey})`;
 }
