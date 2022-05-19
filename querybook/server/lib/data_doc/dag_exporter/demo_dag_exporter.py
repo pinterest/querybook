d0b74c0be5fc1bde15fb6585cd91267b7a130843
Lena Ryoo <lryoo@pinterest.com>
2022-05-18 17:57:28 -0700

    clean

--- querybook/server/lib/data_doc/dag_exporter/demo_dag_exporter.py
+++ querybook/server/lib/data_doc/dag_exporter/demo_dag_exporter.py
@@ -1,39 +1,24 @@
 from lib.data_doc.base_dag_exporter import BaseDAGExporter
 from lib.logger import get_logger
 from lib.form import StructFormField, FormField
-from lib.form import FormFieldType
 
 
 LOG = get_logger(__file__)
 
-
-airflow_dag = """
+aiflow_dag = """
 from datetime import datetime, timedelta
-from textwrap import dedent
 
 from airflow import DAG
 
 from airflow.operators.bash import MySqlOperator
 
 with DAG(
-    "{title}",
-    # These args will get passed on to each operator
-    # You can override them on a per-task basis during operator initialization
-    default_args={
-        "depends_on_past": False,
-        "email": ["airflow@example.com"],
-        "email_on_failure": False,
-        "email_on_retry": False,
-        "retries": 1,
-        "retry_delay": timedelta(minutes=5),
-    },
-    description="{description}",
+    "{}",
+    description="{}",
     schedule_interval=timedelta(days=1),
     start_date=datetime(2021, 1, 1),
     catchup=False,
-    tags=["example"],
-) as dag:
-"""
+) as dag:"""
 
 
 class DemoDAGExporter(BaseDAGExporter):
@@ -47,25 +32,20 @@ class DemoDAGExporter(BaseDAGExporter):
 
     @property
     def dag_exporter_meta(self):
-        # TODO: use FormField
         return StructFormField(
-            input=FormField(description="sample input field"),
-            checkbox=FormField(field_type=FormFieldType.Boolean),
-            select=FormField(
-                field_type=FormFieldType.Select,
-                options=["Sample Option 1", "Sample Option 2"],
-            ),
+            title=FormField(description="dag title"),
+            description=FormField(description="dag description"),
         )
 
     def export(self, nodes, edges, meta):
         try:
-            export_dag = ""
+            export_dag = aiflow_dag.format(meta["title"], meta["description"])
 
             for node in nodes:
                 node_section = '''\n
     query_{id} = r"""{query}"""
 
-    task{id} = MySqlOperator(
+    task_{id} = MySqlOperator(
         dag=dag, task_id="cell_{id}", sql=query_{id}
     )
                 '''.format(
@@ -87,7 +67,7 @@ class DemoDAGExporter(BaseDAGExporter):
 
             return {
                 "type": self.dag_exporter_type,
-                "export": airflow_dag + export_dag,
+                "export": export_dag,
             }
         except Exception as e:
             LOG.info(e)
