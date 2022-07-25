af3a9a2ab8ccdaf39ef050222cde4557df86f7b1
J.C. Zhong <exzjc@hotmail.com>
2022-07-25 14:20:53 -0700

    fix create table sql grammar

--- querybook/webapp/__tests__/lib/sql-helper/sql-checker.test.ts
+++ querybook/webapp/__tests__/lib/sql-helper/sql-checker.test.ts
@@ -26,23 +26,30 @@ describe('getDroppedTables', () => {
     test('drops a table and recreated', () => {
         const query1 = `
         drop table if exists db.table1;
-        create table db.table1 if not exists;
+        create table db.table1;
     `;
         expect(getDroppedTables(query1)).toStrictEqual([]);
 
-        // with `external`
+        // with `if not exists`
         const query2 = `
         drop table if exists db.table1;
-        create external table db.table1 if not exists;
+        create table if not exists db.table1;
     `;
         expect(getDroppedTables(query2)).toStrictEqual([]);
+
+        // with `external`
+        const query3 = `
+        drop table if exists db.table1;
+        create external table if not exists db.table1;
+    `;
+        expect(getDroppedTables(query3)).toStrictEqual([]);
     });
 
     test('drops more tables than recreated', () => {
         const query = `
         drop table if exists db.table1;
         drop table db.table2;
-        create table db.table1 if not exists;
+        create table if not exists db.table1;
     `;
         expect(getDroppedTables(query)).toStrictEqual(['db.table2']);
     });
@@ -52,7 +59,7 @@ describe('getDroppedTables', () => {
         use db;
         drop table if exists table1;
         drop table table2;
-        create table db.table1 if not exists;
+        create table if not exists db.table1;
     `;
         expect(getDroppedTables(query)).toStrictEqual(['db.table2']);
     });
