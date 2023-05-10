9c0ebf9453ae4ec73b0829094a2933f0778c8d30
Najma Moideen <najma@saal.ai>
2023-05-10 09:41:52 +0400

    lint fix

--- querybook/server/lib/result_store/stores/file_store.py
+++ querybook/server/lib/result_store/stores/file_store.py
@@ -60,7 +60,7 @@ class FileReader(BaseReader):
     def start(self):
         pass
 
-    def get_csv_iter(self, number_of_lines: Optional[int]): 
+    def get_csv_iter(self, number_of_lines: Optional[int]):
         return islice(str_to_csv_iter(self.read_raw()), number_of_lines)
 
     def read_lines(self, number_of_lines: int):
