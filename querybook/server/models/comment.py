2309fd0c4ba27586235d078c0265b50c00e8b3f0
meowcodes <meowcodes@gmail.com>
2023-05-12 13:53:21 -0700

    update

--- querybook/server/models/comment.py
+++ querybook/server/models/comment.py
@@ -20,7 +20,12 @@ class Comment(CRUDMixin, Base):
 
     text = sql.Column(sql.Text(length=mediumtext_length))
 
-    has_thread = sql.Column(sql.Boolean, default=False)
+    parent_comment_id = sql.Column(
+        sql.Integer,
+        sql.ForeignKey("comment.id", ondelete="CASCADE"),
+        index=True,
+        nullable=True,
+    )
 
 
 class CommentReaction(CRUDMixin, Base):
@@ -32,11 +37,8 @@ class CommentReaction(CRUDMixin, Base):
         sql.ForeignKey("comment.id", ondelete="CASCADE"),
         index=True,
         nullable=False,
-        unique=True,
     )
-    reaction = sql.Column(
-        sql.String(length=name_length), index=True, unique=True, nullable=False
-    )
+    reaction = sql.Column(sql.String(length=name_length), index=True, nullable=False)
     created_by = sql.Column(
         sql.Integer, sql.ForeignKey("user.id", ondelete="SET NULL"), nullable=True
     )
@@ -51,13 +53,11 @@ class DataTableComment(CRUDMixin, Base):
         sql.ForeignKey("data_table.id", ondelete="CASCADE"),
         index=True,
         nullable=False,
-        unique=True,
     )
     comment_id = sql.Column(
         sql.Integer,
         sql.ForeignKey("comment.id", ondelete="CASCADE"),
         nullable=False,
-        unique=True,
     )
 
     data_table = relationship("DataTable", uselist=False)
@@ -73,36 +73,12 @@ class DataCellComment(CRUDMixin, Base):
         sql.ForeignKey("data_cell.id", ondelete="CASCADE"),
         index=True,
         nullable=False,
-        unique=True,
     )
     comment_id = sql.Column(
         sql.Integer,
         sql.ForeignKey("comment.id", ondelete="CASCADE"),
         nullable=False,
-        unique=True,
     )
 
     data_cell = relationship("DataCell", uselist=False)
     comment = relationship("Comment", uselist=False)
-
-
-class NestedComment(CRUDMixin, Base):
-    __tablename__ = "nested_comment"
-    id = sql.Column(sql.Integer, primary_key=True, autoincrement=True)
-    parent_comment_id = sql.Column(
-        sql.Integer,
-        sql.ForeignKey("comment.id", ondelete="CASCADE"),
-        index=True,
-        nullable=False,
-        unique=True,
-    )
-    child_comment_id = sql.Column(
-        sql.Integer,
-        sql.ForeignKey("comment.id", ondelete="CASCADE"),
-        nullable=False,
-        unique=True,
-    )
-
-    child_comment = relationship(
-        "Comment", foreign_keys="NestedComment.child_comment_id", uselist=False
-    )
