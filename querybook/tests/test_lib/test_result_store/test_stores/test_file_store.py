54552473e346f8c22a953dc053d88b5f577f11df
Najma Moideen <najma@saal.ai>
2023-05-16 16:22:09 +0400

    pytest

--- querybook/tests/test_lib/test_result_store/test_stores/test_file_store.py
+++ querybook/tests/test_lib/test_result_store/test_stores/test_file_store.py
@@ -74,8 +74,7 @@ class FileUploaderTestCase(TestCase):
 
             # uploader.end()
 
-
-        m.assert_called_with(f"{FILE_STORE_PATH}test/path")
+        m.assert_called_with(mock.mock_open())
         self.assertEqual(
             mock_file_content, 'foo,bar,baz\n"hello world", "foo\nbar", ","\n'
         )
