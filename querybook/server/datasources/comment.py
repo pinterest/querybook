dea384b33e49d6d5aa4eb211f6e98737ea91e19f
meowcodes <meowcodes@gmail.com>
2023-06-05 01:31:29 -0400

    backend: add comment endpoints

---
+++ querybook/server/datasources/comment.py
@@ -0,0 +1,93 @@
+from flask_login import current_user
+from app.datasource import register
+from logic import comment as logic
+from querybook.server.app.db import DBSession
+from querybook.server.logic.comment_permission import assert_can_edit_and_delete
+
+
+@register(
+    "/comment/<int:data_cell_id>/",
+    methods=["GET"],
+)
+def get_comments_by_cell_id(data_cell_id: int):
+    return logic.get_comments_by_data_cell_id(data_cell_id=data_cell_id)
+
+
+@register(
+    "/comment/<int:table_id>/",
+    methods=["GET"],
+)
+def get_comments_by_table_id(table_id: int):
+    return logic.get_comments_by_data_table_id(table_id=table_id)
+
+
+@register(
+    "/comment/<int:data_cell_id>/",
+    methods=["POST"],
+)
+def add_comment_to_cell(data_cell_id: int, text):
+    return logic.add_comment_to_data_cell(
+        data_cell_id=data_cell_id, uid=current_user.id, text=text
+    )
+
+
+@register(
+    "/comment/<int:data_table_id>/",
+    methods=["POST"],
+)
+def add_comment_to_table(data_table_id: int, text):
+    return logic.add_comment_to_data_table(
+        data_table_id=data_table_id,
+        uid=current_user.id,
+        text=text,
+    )
+
+
+@register(
+    "/comment/<int:parent_comment_id>/",
+    methods=["POST"],
+)
+def add_thread_comment(parent_comment_id: int, text):
+    return logic.add_thread_comment(
+        parent_comment_id=parent_comment_id, uid=current_user.id, text=text
+    )
+
+
+@register(
+    "/comment/<int:comment_id>/",
+    methods=["PUT"],
+)
+def edit_comment(comment_id: int, text):
+    with DBSession() as session:
+        assert_can_edit_and_delete(comment_id=comment_id, session=session)
+        return logic.edit_comment(comment_id=comment_id, text=text)
+
+
+@register(
+    "/comment/<int:comment_id>/",
+    methods=["DELETE"],
+)
+def remove_comment(comment_id: int):
+    with DBSession() as session:
+        assert_can_edit_and_delete(comment_id=comment_id, session=session)
+        return logic.remove_comment(comment_id=comment_id)
+
+
+# reactions
+@register(
+    "/comment/<int:comment_id>/",
+    methods=["POST"],
+)
+def add_reaction(comment_id: int, reaction):
+    return logic.add_reaction(
+        comment_id=comment_id,
+        reaction=reaction,
+    )
+
+
+@register(
+    "/comment/<int:reaction_id>/",
+    methods=["DELETE"],
+)
+def remove_reaction(reaction_id: int):
+    return logic.remove_reaction(reaction_id=reaction_id)
---
+++ querybook/server/logic/comment.py
@@ -0,0 +1,142 @@
+from querybook.server.app.db import with_session
+from models.comment import Comment, CommentReaction, DataTableComment, DataCellComment
+
+
+@with_session
+def get_comment_by_id(comment_id: int, session=None):
+    return Comment.get(id=comment_id, session=session)
+
+
+@with_session
+def get_all_comment_data_dict_by_id(comment_id: int, session=None):
+    comment = Comment.get(id=comment_id, session=session)
+    child_comments = (
+        session.query(Comment).filter(Comment.parent_comment_id == comment_id).all()
+    )
+    reactions = (
+        session.query(CommentReaction)
+        .filter(CommentReaction.comment_id == comment_id)
+        .all()
+    )
+    comment_dict = comment.to_dict()
+    if child_comments:
+        comment_dict["child_comments"] = [
+            child_comment.to_dict() for child_comment in child_comments
+        ]
+    comment_dict["reactions"] = [reaction.to_dict() for reaction in reactions]
+
+    return comment_dict
+
+
+@with_session
+def get_comments_by_data_cell_id(data_cell_id: int, session=None):
+    cell_comments = (
+        session.query(DataCellComment)
+        .filter(DataCellComment.data_cell_id == data_cell_id)
+        .all()
+    )
+
+    comments = []
+    for cell_comment in cell_comments:
+        comment_dict = get_all_comment_data_dict_by_id(
+            cell_comment.comment_id, session=session
+        )
+        comments.append(comment_dict)
+
+    return comments
+
+
+@with_session
+def get_comments_by_data_table_id(data_table_id: int, session=None):
+    table_comments = (
+        session.query(DataTableComment)
+        .filter(DataTableComment.data_table_id == data_table_id)
+        .all()
+    )
+
+    comments = []
+    for table_comment in table_comments:
+        comment_dict = get_all_comment_data_dict_by_id(
+            table_comment.comment_id, session=session
+        )
+        comments.append(comment_dict)
+
+    return comments
+
+
+@with_session
+def add_comment_to_data_cell(
+    data_cell_id: int, uid: int, text, commit=True, session=None
+):
+    comment = Comment.create(
+        {"created_by": uid, "text": text}, commit=commit, session=session
+    )
+    DataCellComment.create(
+        {"data_cell_id": data_cell_id, "comment_id": comment.id},
+        commit=commit,
+        session=session,
+    )
+    return comment.id
+
+
+@with_session
+def add_comment_to_data_table(
+    data_table_id: int, uid: int, text, commit=True, session=None
+):
+    comment = Comment.create(
+        {"created_by": uid, "text": text}, commit=commit, session=session
+    )
+    DataTableComment.create(
+        {"data_table_id": data_table_id, "comment_id": comment.id},
+        commit=commit,
+        session=session,
+    )
+    return comment.id
+
+
+@with_session
+def add_thread_comment(
+    parent_comment_id: int, uid: int, text, commit=True, session=None
+):
+    comment = Comment.create(
+        {"created_by": uid, "text": text, "parent_comment_id": parent_comment_id},
+        commit=commit,
+        session=session,
+    )
+    return comment.id
+
+
+@with_session
+def edit_comment(comment_id: int, text, commit=True, session=None):
+    return Comment.update(
+        id=comment_id,
+        text=text,
+        commit=commit,
+        session=session,
+    )
+
+
+@with_session
+def remove_comment(comment_id: int, session=None):
+    Comment.delete(
+        comment_id,
+        session=session,
+    )
+
+
+@with_session
+def add_reaction(comment_id: int, reaction: str, commit=True, session=None):
+    reaction = CommentReaction.create(
+        {"comment_id": comment_id, "reaction": reaction},
+        commit=commit,
+        session=session,
+    )
+    return reaction.id
+
+
+@with_session
+def remove_reaction(reaction_id: int, session=None):
+    CommentReaction.delete(
+        reaction_id,
+        session=session,
+    )
