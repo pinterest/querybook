18d229a3a65b584e44fafaefdfaf9a50d13aed45
J.C. Zhong <exzjc@hotmail.com>
2023-06-09 00:00:28 +0000

    upgrade langchain

--- querybook/server/datasources/ai_assistant.py
+++ querybook/server/datasources/ai_assistant.py
@@ -8,6 +8,6 @@ from lib.ai_assistant import ai_assistant
 @register("/ai/query_title/", methods=["POST"], custom_response=True)
 def generate_query_title(query):
     title_stream = ai_assistant.generate_title_from_query(
-        query=query, user_id=current_user.id if current_user else None
+        query=query, user_id=current_user.id
     )
     return Response(title_stream, mimetype="text/event-stream")
