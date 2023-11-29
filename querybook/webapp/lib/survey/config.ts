9e4e7e821e7dea7cc6c95af5ed66d4bc41ec9f74
czgu <cgu@pinterest.com>
2023-11-28 18:26:25 -0800

    Fix lint, package version, and survey config

--- querybook/webapp/lib/survey/config.ts
+++ querybook/webapp/lib/survey/config.ts
@@ -4,14 +4,19 @@ import type { ISurveyConfig } from './types';
 const surveyConfig = PublicConfig.survey;
 
 export const SURVEY_CONFIG: Record<string, ISurveyConfig> = {};
-surveyConfig?.surfaces.forEach((surface) => {
+surveyConfig?.surfaces?.forEach((surface) => {
     SURVEY_CONFIG[surface.surface] = {
         surface: surface.surface,
         responseCooldown:
-            surface.response_cooldown ?? surveyConfig.global_response_cooldown,
+            surface.response_cooldown ??
+            surveyConfig.global_response_cooldown ??
+            2592000, // 30 days
         triggerCooldown:
-            surface.trigger_cooldown ?? surveyConfig.global_trigger_cooldown,
-        maxPerWeek: surface.max_per_week ?? surveyConfig.global_max_per_week,
-        maxPerDay: surface.max_per_day ?? surveyConfig.global_max_per_day,
+            surface.trigger_cooldown ??
+            surveyConfig.global_trigger_cooldown ??
+            600, // 10 minutes
+        maxPerWeek:
+            surface.max_per_week ?? surveyConfig.global_max_per_week ?? 3,
+        maxPerDay: surface.max_per_day ?? surveyConfig.global_max_per_day ?? 1,
     };
 });
