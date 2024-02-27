4d41106157bfdaf50bd201877688e259eb0884da
Adam Struck <adamjstruck@gmail.com>
2024-02-26 20:57:03 -0800

    fix: schema filtering in table search modal (#1348)
    * fix: schema filtering for table search
    
    * revert unnecessary changes

--- querybook/webapp/components/Search/SearchSchemaSelect.tsx
+++ querybook/webapp/components/Search/SearchSchemaSelect.tsx
@@ -1,7 +1,5 @@
 import React, { useMemo, useCallback } from 'react';
 import AsyncSelect from 'react-select/async';
-import { useSelector } from 'react-redux';
-import { IStoreState } from 'redux/store/types';
 import {
     makeReactSelectStyle,
     asyncReactSelectStyles,
@@ -10,6 +8,7 @@ import {
 import { SearchSchemaResource } from 'resource/search';
 
 interface ISearchSchemaSelectProps {
+    metastoreId: number;
     schema?: string[];
     updateSearchFilter: (key: string, value: string[]) => void;
 }
@@ -22,11 +21,8 @@ const tableReactSelectStyle = makeReactSelectStyle(
 export const SearchSchemaSelect: React.FC<ISearchSchemaSelectProps> = ({
     updateSearchFilter,
     schema,
+    metastoreId,
 }) => {
-    const currentMetastoreId = useSelector(
-        (state: IStoreState) => state.environment.currentEnvironmentId
-    );
-
     const handleUpdateSearchFilter = useCallback((option: IOptions<string>) => {
         updateSearchFilter(
             'schema',
@@ -34,17 +30,20 @@ export const SearchSchemaSelect: React.FC<ISearchSchemaSelectProps> = ({
         );
     }, []);
 
-    const loadOptions = useCallback(async (value) => {
-        const searchRequest = await SearchSchemaResource.getMore({
-            metastore_id: currentMetastoreId,
-            name: value,
-        });
+    const loadOptions = useCallback(
+        async (value) => {
+            const searchRequest = await SearchSchemaResource.getMore({
+                metastore_id: metastoreId,
+                name: value,
+            });
 
-        return searchRequest.data.results.map((schema) => ({
-            label: schema.name,
-            value: schema.name,
-        }));
-    }, []);
+            return searchRequest.data.results.map((schema) => ({
+                label: schema.name,
+                value: schema.name,
+            }));
+        },
+        [metastoreId, schema]
+    );
 
     const selectedSchemaItems = useMemo(
         () =>
