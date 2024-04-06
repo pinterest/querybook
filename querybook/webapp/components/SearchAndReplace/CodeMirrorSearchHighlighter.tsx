35073850d0d7e5d522eac761f37197bbbd09d211
Adam Struck <adamjstruck@gmail.com>
2024-04-05 21:47:39 -0700

    fix: move to first match on search term change (#1416)
    * fix: update highlight/focus to first match on search term change
    
    * pr comments

--- querybook/webapp/components/SearchAndReplace/CodeMirrorSearchHighlighter.tsx
+++ querybook/webapp/components/SearchAndReplace/CodeMirrorSearchHighlighter.tsx
@@ -61,7 +61,7 @@ export const CodeMirrorSearchHighlighter: React.FC<{
                 }
             );
         }
-    }, [currentSearchResultIndex]);
+    }, [currentSearchItem]);
 
     return null;
 };
