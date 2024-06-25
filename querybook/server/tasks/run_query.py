1fc93d276b87224323dcf4ba82db0b57a0a3cd1e
rongzhang <rongzhang@pinterest.com>
2024-06-25 16:12:35 +0000

    remove unused line

--- querybook/server/tasks/run_query.py
+++ querybook/server/tasks/run_query.py
@@ -41,9 +41,6 @@ def run_query_task(
         executor = create_executor_from_execution(
             query_execution_id, celery_task=self, execution_type=execution_type
         )
-        import time
-
-        time.sleep(5)  # Introduce a 10-second delay here
         run_executor_until_finish(self, executor)
     except SoftTimeLimitExceeded:
         # SoftTimeLimitExceeded
