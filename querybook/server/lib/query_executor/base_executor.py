1092268c4c4fa4caeb1e122dfcbdb7b6f0a52a52
Victor Zhang <rongzhang@pinterest.com>
2024-07-08 13:53:13 -0400

    feat: pass api_access_token to executor client (#1473)
    * feat: pass api_access_token to executor client
    
    * use execution metadata to store access token flag

--- querybook/server/lib/query_executor/base_executor.py
+++ querybook/server/lib/query_executor/base_executor.py
@@ -528,6 +528,18 @@ class QueryExecutorBaseClass(metaclass=ABCMeta):
         self._client = None
         self._cursor = None
 
+        query_execution_metadata = (
+            qe_logic.get_query_execution_metadata_by_execution_id(
+                self._query_execution_id
+            )
+        )
+        if query_execution_metadata:
+            self._used_api_token = query_execution_metadata.execution_metadata.get(
+                "used_api_token", False
+            )
+        else:
+            self._used_api_token = False
+
     def __del__(self):
         del self._logger
         del self._cursor
