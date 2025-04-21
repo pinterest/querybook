21f4bc0bb3da809fb0356f25eb5019f440c887f3
J.C. Zhong <exzjc@hotmail.com>
2025-04-21 20:41:24 +0000

    fix: change_log_last_viewed not written when Show Full View setting enabled

--- querybook/webapp/components/EnvironmentAppRouter/modalRoute/ChangeLogRoute.tsx
+++ querybook/webapp/components/EnvironmentAppRouter/modalRoute/ChangeLogRoute.tsx
@@ -5,8 +5,6 @@ import { RouteComponentProps } from 'react-router-dom';
 import { ChangeLog } from 'components/ChangeLog/ChangeLog';
 import { useBrowserTitle } from 'hooks/useBrowserTitle';
 import { useModalRoute } from 'hooks/useModalRoute';
-import localStore from 'lib/local-store';
-import { CHANGE_LOG_KEY, ChangeLogValue } from 'lib/local-store/const';
 import history from 'lib/router-history';
 import { Modal } from 'ui/Modal/Modal';
 
@@ -16,16 +14,8 @@ const ChangeLogRoute: React.FunctionComponent<RouteComponentProps> = ({
     useBrowserTitle('Change Log');
     const isModal = useModalRoute(location);
 
-    const handleHide = () => {
-        localStore.set<ChangeLogValue>(
-            CHANGE_LOG_KEY,
-            moment().format('YYYY-MM-DD')
-        );
-        history.goBack();
-    };
-
     return isModal ? (
-        <Modal onHide={handleHide} title="Change Log">
+        <Modal onHide={history.goBack} title="Change Log">
             <ChangeLog />
         </Modal>
     ) : (
