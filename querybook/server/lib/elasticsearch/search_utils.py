b275774cd701b1cc0d2a55e99de47acc0390bf9f
jiaxuan.lee <jiaxuan.lee@shopline.com>
2025-07-03 18:50:42 +0800

    disable es

--- querybook/server/lib/elasticsearch/search_utils.py
+++ querybook/server/lib/elasticsearch/search_utils.py
@@ -1,6 +1,6 @@
 from typing import Dict, Union
 
-from elasticsearch import Elasticsearch, RequestsHttpConnection
+# from elasticsearch import Elasticsearch, RequestsHttpConnection
 
 from env import QuerybookSettings
 from lib.config import get_config_value
@@ -16,26 +16,26 @@ ES_CONFIG = get_config_value("elasticsearch")
 def get_hosted_es():
     hosted_es = None
 
-    if QuerybookSettings.ELASTICSEARCH_CONNECTION_TYPE == "naive":
-        hosted_es = Elasticsearch(hosts=QuerybookSettings.ELASTICSEARCH_HOST)
-    elif QuerybookSettings.ELASTICSEARCH_CONNECTION_TYPE == "aws":
-        # TODO: generialize aws region setup
-        from boto3 import session as boto_session
-        from lib.utils.assume_role_aws4auth import AssumeRoleAWS4Auth
+    # if QuerybookSettings.ELASTICSEARCH_CONNECTION_TYPE == "naive":
+    #     hosted_es = Elasticsearch(hosts=QuerybookSettings.ELASTICSEARCH_HOST)
+    # elif QuerybookSettings.ELASTICSEARCH_CONNECTION_TYPE == "aws":
+    #     # TODO: generialize aws region setup
+    #     from boto3 import session as boto_session
+    #     from lib.utils.assume_role_aws4auth import AssumeRoleAWS4Auth
 
-        credentials = boto_session.Session().get_credentials()
-        auth = AssumeRoleAWS4Auth(
-            credentials,
-            QuerybookSettings.AWS_REGION,
-            "es",
-        )
-        hosted_es = Elasticsearch(
-            hosts=QuerybookSettings.ELASTICSEARCH_HOST,
-            http_auth=auth,
-            connection_class=RequestsHttpConnection,
-            use_ssl=True,
-            verify_certs=True,
-        )
+    #     credentials = boto_session.Session().get_credentials()
+    #     auth = AssumeRoleAWS4Auth(
+    #         credentials,
+    #         QuerybookSettings.AWS_REGION,
+    #         "es",
+    #     )
+    #     hosted_es = Elasticsearch(
+    #         hosts=QuerybookSettings.ELASTICSEARCH_HOST,
+    #         http_auth=auth,
+    #         connection_class=RequestsHttpConnection,
+    #         use_ssl=True,
+    #         verify_certs=True,
+    #     )
     return hosted_es
 
 
@@ -59,7 +59,8 @@ def _parse_results(results, get_count):
 def get_matching_objects(query: Union[str, Dict], index_name, get_count=False):
     result = None
     try:
-        result = get_hosted_es().search(index=index_name, body=query)
+        # result = get_hosted_es().search(index=index_name, body=query)
+        result = []
     except Exception as e:
         LOG.warning("Got ElasticSearch exception: \n " + str(e))
 
@@ -72,7 +73,8 @@ def get_matching_objects(query: Union[str, Dict], index_name, get_count=False):
 def get_matching_suggestions(query: Union[str, Dict], index_name: str):
     result = None
     try:
-        result = get_hosted_es().search(index=index_name, body=query)
+        # result = get_hosted_es().search(index=index_name, body=query)
+        result = []
     except Exception as e:
         LOG.warning(e)
     finally:
