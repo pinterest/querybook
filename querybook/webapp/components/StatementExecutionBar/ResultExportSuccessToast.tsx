eedeaf2842923ae4ac96fe67cafd97788f910caa
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2026-07-31 19:58:38 -0600

    feat: show messages for URL result exporters

--- querybook/webapp/components/StatementExecutionBar/ResultExportSuccessToast.tsx
+++ querybook/webapp/components/StatementExecutionBar/ResultExportSuccessToast.tsx
@@ -1,38 +1,58 @@
 import React from 'react';
 import toast from 'react-hot-toast';
 
-import { IQueryExecutionExportStatusInfo } from 'const/queryExecution';
+import {
+    IQueryExecutionExportStatusInfo,
+    IUrlExportResult,
+} from 'const/queryExecution';
 import { Button } from 'ui/Button/Button';
 import { CopyPasteModal } from 'ui/CopyPasteModal/CopyPasteModal';
+import { Message } from 'ui/Message/Message';
 import { Modal } from 'ui/Modal/Modal';
 
-const UrlModal: React.FunctionComponent<{
-    url: string;
-    title: string;
-    onHide: () => any;
-}> = ({ url, title, onHide }) => (
-    <Modal onHide={onHide} title={title}>
-        <div className="flex-center mv24">
-            <Button title="View Export" onClick={() => window.open(url)} />
-        </div>
-    </Modal>
-);
+const openExport = (url: string) => window.open(url);
+
+export const UrlExportRedirect: React.FunctionComponent<{
+    exportResult: IUrlExportResult;
+    onHide: () => void;
+    open?: (url: string) => void;
+}> = ({
+    exportResult: { url, message, message_type: messageType = 'warning' },
+    onHide,
+    open = openExport,
+}) => {
+    return (
+        <Modal onHide={onHide} title="Export Complete">
+            {message ? <Message type={messageType}>{message}</Message> : null}
+            <div className="flex-center mv24">
+                <Button title="View Export" onClick={() => open(url)} />
+            </div>
+        </Modal>
+    );
+};
 
 export const ResultExportSuccessToast = (
-    data: IQueryExecutionExportStatusInfo
+    data: IQueryExecutionExportStatusInfo,
 ) => {
     const { result: exportedInfo, task_id: taskId } = data;
     const title = 'Export Complete';
 
+    if (!exportedInfo) {
+        return null;
+    }
+
     return exportedInfo.type === 'url' ? (
-        <UrlModal
-            url={exportedInfo.info}
-            title={title}
+        <UrlExportRedirect
+            exportResult={
+                typeof exportedInfo.info === 'string'
+                    ? { url: exportedInfo.info }
+                    : exportedInfo.info
+            }
             onHide={() => toast.dismiss(taskId)}
         />
     ) : (
         <CopyPasteModal
-            text={exportedInfo.info}
+            text={String(exportedInfo.info)}
             title={title}
             onHide={() => toast.dismiss(taskId)}
         />
